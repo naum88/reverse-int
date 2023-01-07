fe84c553f0502ad3cf075d553608ae93beee51e8
Andrei Naumenko <6110814n@gmail.com>
2023-01-07 16:13:28 +0300

    init: reverse init

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+    let num = Math.abs(n);
+  //console.log(num);
+  let str = String(num).split('').reverse().join('');
+  console.log(+str);
+  return Number(str); 
 }
